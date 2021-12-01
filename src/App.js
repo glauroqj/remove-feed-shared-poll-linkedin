4be075618ec64e1ec4a4147d0fb4d3d307ca352e
glauroqj <glauro.juliani@hotmail.com>
2021-12-01 18:34:05 -0300

    add: start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <div className="linkeclean-app">
+
+      <div className="footer">
+        Feito com amor por <a href="https://www.linkedin.com/in/glauro-juliani/" target="new">Glauro Juliani</a> <b>0.2.4</b>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
